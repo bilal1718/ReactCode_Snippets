6ab2d0dbb0d14d0edb699e2c206d4b3d6cb18db1
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-16 13:03:21 +0500

    Update useState_Hook_inAction.jsx

--- useState_Hook_inAction.jsx
+++ useState_Hook_inAction.jsx
@@ -1,1 +1,13 @@
 
+import { useState } from "react";
+
+export default function App() {
+  const [giftCard, setGiftCard] = useState(
+    {
+        firstName: "Jennifer",
+        lastName: "Smith",
+        text: "Free dinner for 4 guests",
+        valid: true,
+        instructions: "To use your coupon, click the button below.",
+    }
+  );
