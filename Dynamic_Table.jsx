746e3d71894ed308ad9e5aee2f312a9ea525ccd6
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-17 15:54:05 +0500

    Update Dynamic_Table.jsx

--- Dynamic_Table.jsx
+++ Dynamic_Table.jsx
@@ -1,1 +1,16 @@
 
+import React from 'react';
+
+const DynamicTable = ({ data }) => {
+  const headers = data.length > 0 ? Object.keys(data[0]) : [];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map(header => (
+            <th key={header}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
