6fdbbdfef64cdb46fad79636333c20aa11c56055
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-18 15:47:09 +0500

    Update CurrentTime.js
    Applying css

--- CurrentTime.js
+++ CurrentTime.js
@@ -1,1 +1,9 @@
-
+export default function Clock() {
+  const timeStyle = {
+    fontFamily: 'sans-serif',
+    width: 'max-content',
+    height: 'max-content',
+    fontSize: '3rem',
+    color: 'white',
+    backgroundColor: 'black',
+  };
