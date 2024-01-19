8ec69ba6e8bd1b162af33cce0175d48e160be123
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-19 12:11:21 +0500

    Update Restrict_Api_Calls.js

--- Restrict_Api_Calls.js
+++ Restrict_Api_Calls.js
@@ -1,1 +1,21 @@
 
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const [jobIds, setJobIds] = useState([]);
+  const [jobs, setJobs] = useState([]);
+  const [startIndex, setStartIndex] = useState(6);
+
+  useEffect(() => {
+    const fetchJobIds = async () => {
+      try {
+        const response = await fetch('https://hacker-news.firebaseio.com/v0/jobstories.json');
+        const jobIdsData = await response.json();
+        setJobIds(jobIdsData.slice(startIndex)); // Get the first six job IDs
+      } catch (error) {
+        console.error('Error fetching job IDs:', error);
+      }
+    };
+
+    fetchJobIds();
+  }, []);
