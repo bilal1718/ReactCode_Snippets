42ced2fc2fe7b17460cdb5b41250d6595ee21ddc
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-20 13:54:27 +0500

    Update progress_bar.js

--- progress_bar.js
+++ progress_bar.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+
+
+const ProgressBar = ({ percent=10 }) => {
+  const progressBarStyle = {
+    width: '100%',
+    height: '20px',
+    borderRadius: '5px',
+    backgroundColor: 'lightgrey',
+    overflow: 'hidden',
+  };
+
+  const progressStyle = {
+    width: `${percent}%`,
+    height: '100%',
+    borderRadius: '5px',
+    backgroundColor: 'green',
+  };
+
+ 
