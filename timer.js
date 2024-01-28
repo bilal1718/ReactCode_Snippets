9a0278e6b0f31b5b9f2e87968e2176cda00f7799
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-28 15:38:10 +0500

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const minutes = Math.floor(elapsedTime / 60);
+  const seconds = elapsedTime % 60;
+
+  return (
+    <div>
+      {`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}
+    </div>
+  );
+};
+
+export default App;
