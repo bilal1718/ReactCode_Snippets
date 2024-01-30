d1ba6c32028864569f71c365800c33a29d48e5ab
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-30 10:26:05 +0500

    Update products.jsx

--- ShoppingCart/products.jsx
+++ ShoppingCart/products.jsx
@@ -1,1 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 
+const Products = ({products,AddtoProduct}) => {
+  return (
+    <div>
+        <h1>Products</h1>
+        <Link to="cart"><button>Cart</button></Link>
+        <div className="products">
+      {products.map((product) => (
+        <div key={product.id} className="product">
+          <h1>{product.title}</h1>
+          <img src={product.images[0]} className="image" alt="" />
+          <button onClick={()=>AddtoProduct(product)} className="button">Add to Cart</button>
+        </div>
+      ))}
+      </div>
+    </div>
+  )
+}
+
+export default Products
