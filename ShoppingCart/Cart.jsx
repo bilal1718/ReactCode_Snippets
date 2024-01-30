6bb11f44e074a5dea89a8df2784e3e1d8b1ce2ac
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-30 10:25:29 +0500

    Update Cart.jsx

--- ShoppingCart/Cart.jsx
+++ ShoppingCart/Cart.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
 
+const Cart = ({cartItems,removeProduct}) => {
+  return (
+<div className="products">
+      {cartItems.map((product) => (
+        <div key={product.product.id} className="product">
+          <h1>{product.product.title}</h1>
+          <h1>Quantity : {product.quantity}</h1>
+          <img src={product.product.images[0]} className="image" alt="" />
+          <button className='remove' onClick={()=>removeProduct(product)}>Remove</button>
+        </div>
+      ))}
+      </div>
+  )
+}
+
+export default Cart
