8c01a23248e4274624d85ff1a7a836290c31f8a0
Muhammad Bilal <muhbil171845@gmail.com>
2024-01-31 12:36:27 +0500

    Update Radio.js

--- React_TopLevel_Api/Radio.js
+++ React_TopLevel_Api/Radio.js
@@ -1,1 +1,32 @@
 
+import * as React from "react";
+import "./styles.css";
+
+export const RadioGroup = ({ onChange, selected, children }) => {
+  const RadioOptions = React.Children.map(children, (child) =>
+    React.cloneElement(child, {
+      checked: child.props.value === selected,
+      onChange,
+    })
+  );
+  return <div className="RadioGroup">{RadioOptions}</div>;
+};
+export const RadioOption = ({ value, checked, onChange, children }) => {
+  
+  const handleChange = (e) => {
+    onChange(e.target.value);
+  };
+  return (
+    <div className="RadioOption">
+      <input
+        id={value}
+        type="radio"
+        name={value}
+        value={value}
+        checked={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor={value}>{children}</label>
+    </div>
+  );
+};
