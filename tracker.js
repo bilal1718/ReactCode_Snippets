d0896f6e570f802c49f7281faec8d8505b95158e
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-01 09:51:53 +0500

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -1,1 +1,64 @@
 
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const MousePosition = ({ render }) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const handleMousePositionChange = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMousePositionChange);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMousePositionChange);
+    };
+  }, []);
+
+  // Pass the mousePosition to the render function
+  return render(mousePosition);
+};
+
+const PanelMouseLogger = () => (
+  <MousePosition
+    render={(position) => (
+      <div className="BasicTracker">
+        <p>Mouse position:</p>
+        <div className="Row">
+          <span>x: {position.x}</span>
+          <span>y: {position.y}</span>
+        </div>
+      </div>
+    )}
+  />
+);
+
+const PointMouseLogger = () => (
+  <MousePosition
+    render={(position) => (
+      <p>
+        ({position.x}, {position.y})
+      </p>
+    )}
+  />
+);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">Little Lemon Restaurant 🍕</header>
+      <PanelMouseLogger />
+      <PointMouseLogger />
+    </div>
+  );
+}
+
+export default App;
