8ffa683be8f392218408906be29ccc91374e10e2
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-04 20:32:54 +0500

    Update checkClick.js

--- checkClick.js
+++ checkClick.js
@@ -1,1 +1,11 @@
 
+const App = () => {
+  function CheckClick(){
+    console.log("Clicked!")
+  }
+  return (
+    <button data-testid='button' onClick={CheckClick}>Click me!</button>
+  )
+}
+
+export default App
