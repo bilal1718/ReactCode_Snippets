17b5d8032a8a1192f95f7c048e732dcebd2fe595
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-04 20:30:42 +0500

    Update tempConverter.js

--- tempConverter.js
+++ tempConverter.js
@@ -1,1 +1,29 @@
 
+import {React,useState} from 'react'
+const App = () => {
+  const [temp,setTemp]=useState()
+  function convertToKelvin(celsius){
+    return celsius + 273.15;
+  }
+  function convertToFarenhiet(celsius){
+    return celsius * (9/5) + 32;
+  }
+  return (
+    <div>
+      <form>
+        <input
+          data-testid='input-id'
+          name="input"
+          type="number"
+          value={temp} onChange={(e)=>setTemp(Number(e.target.value))}
+        />
+        <label for="input" >°C</label>
+      </form>
+      <p data-testid='output'>
+         {temp ? `${temp} is ${convertToFarenhiet(temp)}F and ${convertToKelvin(temp)}K ` : ""}
+      </p>
+    </div>
+  );
+};
+
+export default App;
