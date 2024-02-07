cc43fcc24f895eb8a1d64fdf0361565e0e1a4269
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-07 17:12:16 +0500

    Update To_Do_List.js

--- To_Do_List.js
+++ To_Do_List.js
@@ -1,1 +1,14 @@
 
+import {useState} from "react"
+export default function App() {
+  const [tasks,setTasks]=useState([]);
+  const [input,setInput]=useState('');
+  function submit(e){
+    e.preventDefault();
+    setTasks([...tasks,input]);
+    setInput('');
+  }
+  function deleteTask(index) {
+  const updatedTasks = tasks.filter((_, i) => i !== index);
+  setTasks(updatedTasks);
+}
