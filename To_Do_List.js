39d24ecfa4188b081ea9cb87a4540e2d72634478
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-07 17:12:38 +0500

    Update To_Do_List.js

--- To_Do_List.js
+++ To_Do_List.js
@@ -1,14 +1,23 @@
-
-import {useState} from "react"
-export default function App() {
-  const [tasks,setTasks]=useState([]);
-  const [input,setInput]=useState('');
-  function submit(e){
-    e.preventDefault();
-    setTasks([...tasks,input]);
-    setInput('');
-  }
-  function deleteTask(index) {
-  const updatedTasks = tasks.filter((_, i) => i !== index);
-  setTasks(updatedTasks);
+return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input value={input} onChange={(e)=>setInput(e.target.value)} type="text" placeholder="Add your task" />
+        <div>
+          <button onClick={submit}>Submit</button>
+        </div>
+      </div>
+      <ul>
+      <ul>
+  {tasks.map((task, index) => (
+    <li key={index}>
+      <span>{task}</span>
+      <button onClick={()=>deleteTask(index)}>Delete</button>
+    </li>
+  ))}
+</ul>
+        
+      </ul>
+    </div>
+  );
 }
