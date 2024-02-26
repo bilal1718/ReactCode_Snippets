3318ea2903b0f11a36616fdd6665d89827fa434d
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-26 18:52:28 +0500

    Update localStorage.jsx

--- localStorage.jsx
+++ localStorage.jsx
@@ -1,1 +1,33 @@
 
+import React from 'react';
+
+const App = () => {
+  const [inputValue, setInputValue] = React.useState("");
+
+  React.useEffect(() => {
+    const storedValue = localStorage.getItem("inputValue");
+    if (storedValue) {
+      setInputValue(storedValue);
+    }
+  }, []);
+
+  const handleInputChange = (e) => {
+
+    const value = e.target.value;
+    setInputValue(value);
+    localStorage.setItem("inputValue", value);
+  };
+
+  return (
+    <div>
+      <input
+        value={inputValue}
+        onChange={handleInputChange}
+        data-testid='input-id'
+        type="text"
+      />
+    </div>
+  );
+};
+
+export default App;
