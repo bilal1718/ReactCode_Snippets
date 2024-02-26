ddb24c49acfa56ef67e8a388f3aac52a18430d35
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-26 18:34:53 +0500

    Create Error_Page.jsx

---
+++ Error_Page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ErrorPage => (props) = 
+  <div>
+    <h1>Errors in Programming</h1>
+    <p>Everyone makes mistakes, including programmers!</p>
+    <ul>
+      <li>Syntax Errors: These occur when the code is written incorrectly, and the compiler is unable to understand it. Examples include missing semicolons, quotes, or parentheses.
+      <li>Logic Errors: These occur when the code is written correctly, but does not produce the expected result.
+      <li>Run-time Errors: These occur when the code is executed, and something unexpected happens, such as a division by zero error.
+    </ul>
+  <p>The key to fixing errors is to identify the cause, and then make the necessary corrections. This can involve using debugging tools, or simply carefully reading the code and understanding what is happening.
+  </p>
+  </div>
+
+export deflaut ErrorPage
