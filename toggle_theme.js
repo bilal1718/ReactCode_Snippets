8b2ae9837e588e8a9dd6fcb3ead97b116fa8a410
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-28 11:59:11 +0500

    Update toggle_theme.js

--- toggle_theme.js
+++ toggle_theme.js
@@ -1,1 +1,29 @@
 
+import React,{useState} from "react"
+export const useTheme = () => {
+  const [theme,setTheme]=useState('light')
+  const toggleTheme=()=>{
+     setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+  
+  return {
+    theme,toggleTheme
+  }
+}
+
+const App = () => {
+  const {theme, toggleTheme} = useTheme();
+  return (
+    <div 
+      style={{ 
+        height: '100vh',
+        transition: '0.3s ease-in',
+        backgroundColor: theme === 'light' ? 'white' : 'black'
+      }}
+    >
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
+}
+
+export default App;
