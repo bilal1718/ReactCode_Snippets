aa1d45227bad05e26586663cd6cb78cd14fa6d11
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-29 18:51:42 +0500

    Update wordCounter.js

--- wordCounter.js
+++ wordCounter.js
@@ -1,1 +1,13 @@
 
+import React, { useState } from "react";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+
+  const countWords = (e) => {
+    const text = e.target.value;
+    setInput(text);
+    const words = text.split(/\s+/).filter((word) => word.length > 0);
+    setCount(words.length);
+  };
