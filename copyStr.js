25b48057b43b3be13bc4858e8cf4c059011e6f1d
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-01 11:59:59 +0500

    Update copyStr.js

--- copyStr.js
+++ copyStr.js
@@ -1,1 +1,14 @@
+import {useState} from "react"
+import styled from "styled-components";
+
+const CopyToClipboard = ({ str = 'bilal' }) => {
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(str)
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000)
+  }
 
