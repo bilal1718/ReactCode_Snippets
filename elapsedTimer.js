b4185b6941eb424ea88c5ab6f9d831acb7be5725
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-05 12:38:14 +0500

    Update elapsedTimer.js

--- elapsedTimer.js
+++ elapsedTimer.js
@@ -14,3 +14,11 @@ const App = () => {
   const minutes = Math.floor(elapsedTime / 60);
   const seconds = elapsedTime % 60;
   
+return (
+    <div>
+      {`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}
+    </div>
+  );
+};
+
+export default App;
