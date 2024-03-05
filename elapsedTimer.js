701f983e17c21447263ab94e15d47553965a5e90
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-05 12:37:58 +0500

    Update elapsedTimer.js

--- elapsedTimer.js
+++ elapsedTimer.js
@@ -1,1 +1,16 @@
+import React, { useState, useEffect } from "react";
 
+const App = () => {
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const minutes = Math.floor(elapsedTime / 60);
+  const seconds = elapsedTime % 60;
+  
