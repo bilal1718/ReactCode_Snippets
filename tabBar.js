d2d4292e77a08845b427fb1e21c708efb0c6e0ae
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-07 20:07:30 +0500

    Update tabBar.js

--- tabBar.js
+++ tabBar.js
@@ -1,55 +1,60 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import {useState} from 'react'
-
 
 const person = {
   name: 'John Doe',
   age: 32,
-  occupation: 'Developer'
+  occupation: 'Developer',
 };
 
 const address = {
   street: '1234 Main St',
   city: 'San Francisco',
   state: 'CA',
-  zip: '94107'
+  zip: '94107',
 };
 
-
 const Tabs = () => {
-  const [personTab,setPersonTab]=useState(false);
-  const [addressTab,setAddressTab]=useState(false);
-  const handleTab=()=>{
-  setAddressTab(false);
-  setPersonTab(true);
-}
-const handleTab2=()=>{
-  setAddressTab(true);
-  setPersonTab(false);
+  const [activeTab, setActiveTab] = useState('person');
 
-}
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <Container>
       <TabList>
-        <Tab onClick={()=>handleTab()} data-testid="person-tab">
+        <Tab
+          onClick={() => handleTabClick('person')}
+          active={activeTab === 'person'}
+          data-testid="person-tab"
+        >
           Person
         </Tab>
-        <Tab onClick={()=>handleTab2()} data-testid="address-tab">
+        <Tab
+          onClick={() => handleTabClick('address')}
+          active={activeTab === 'address'}
+          data-testid="address-tab"
+        >
           Address
         </Tab>
       </TabList>
       <TabContent>
-         {personTab ?  <PersonContainer data-testid="person-container">
+        {activeTab === 'person' && (
+          <PersonContainer data-testid="person-container">
             <p>Name: {person.name}</p>
             <p>Age: {person.age}</p>
             <p>Occupation: {person.occupation}</p>
-          </PersonContainer> : ""}
-          {addressTab ? < AddressContainer className="AddressContainer" data-testid="address-container">
+          </PersonContainer>
+        )}
+        {activeTab === 'address' && (
+          <AddressContainer className="AddressContainer" data-testid="address-container">
             <p className="TabContent">Street: {address.street}</p>
             <p className="TabContent">City: {address.city}</p>
             <p className="TabContent">State: {address.state}</p>
             <p className="TabContent">Zip: {address.zip}</p>
-          </AddressContainer> : ""} 
+          </AddressContainer>
+        )}
       </TabContent>
     </Container>
   );
@@ -76,8 +81,8 @@ const Tab = styled.li`
   padding: 10px 20px;
   cursor: pointer;
   font-weight: bold;
-  color: ${props => (props.active ? 'black' : 'gray')};
-  background-color: ${props => (props.active ? 'lightgray' : 'white')};
+  color: ${(props) => (props.active ? 'black' : 'gray')};
+  background-color: ${(props) => (props.active ? 'lightgray' : 'white')};
 `;
 
 const TabContent = styled.div`
