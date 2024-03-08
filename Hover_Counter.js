8ec6324600ab4d6e7301172d4f082e5217ebf763
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-08 12:46:41 +0500

    Update Hover_Counter.js

--- Hover_Counter.js
+++ Hover_Counter.js
@@ -1,1 +1,15 @@
 
+import { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button data-testid='button' onMouseEnter={() => setCount(count + 1)}>Hover Me</button>
+      <h1 data-testid='count'>{count}</h1>
+    </div>
+  );
+};
+
+export default App;
