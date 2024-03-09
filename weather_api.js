4c79beeada5b62363dbfa5dc6db41a2193a13436
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-09 12:00:21 +0500

    Update weather_api.js

--- weather_api.js
+++ weather_api.js
@@ -5,3 +5,11 @@ function App() {
   const [weatherData, setWeatherData] = useState({});
   const [city, setCity] = useState("");
   const apiKey = `d3b64dbcb796fa06aed6022a6af9bbd6`;
+useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      });
+  }, [city]);
