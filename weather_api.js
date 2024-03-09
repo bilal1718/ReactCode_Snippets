831fc6db899128b574c43d0d4ec88843cb8535cb
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-09 12:00:08 +0500

    Update weather_api.js

--- weather_api.js
+++ weather_api.js
@@ -1,1 +1,7 @@
 
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("");
+  const apiKey = `d3b64dbcb796fa06aed6022a6af9bbd6`;
