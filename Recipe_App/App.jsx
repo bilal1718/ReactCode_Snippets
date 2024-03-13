bc3e6db46d7149e3d98e38d4f2f258d569bb2025
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-13 15:51:44 +0500

    Update App.jsx

--- Recipe_App/App.jsx
+++ Recipe_App/App.jsx
@@ -19,3 +19,86 @@ function RecipeCard({ recipe, index }) {
     const brightness = (parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) / 1000;
     return brightness >= 128 ? "black" : "white";
   };
+const toggleTags = () => {
+    setShowMoreTags((prev)=>!prev);
+    if (showMoreTags) {
+      setNumTagsDisplayed((prev) => prev + 5);
+    }
+  };
+
+  const getState = () => {
+    setInfoOn((prev) => !prev);
+    setIngredientOn((prev) => !prev);
+  };
+
+  return (
+    <div className="recipe-card" key={index}>
+      <div
+        style={{
+          backgroundImage: `url(${recipe.recipe.image})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "50% 50%",
+          backgroundSize: "cover",
+          height: 150,
+        }}
+      />
+      <div className="recipe-card__body" key={index}>
+        <h1 className="recipe-card__heading">{recipe.recipe.label}</h1>
+        <h2 className="recipe-card__subhead">
+          {recipe.recipe.healthLabels.slice(0, numTagsDisplayed).map((label) => (
+            <span
+              className={`badge`}
+              style={{
+                backgroundColor: getRandomColor(),
+                color: getContrastColor(getRandomColor()),
+              }}
+            >
+              {label}
+            </span>
+          ))}
+          {recipe.recipe.healthLabels.length > numTagsDisplayed && (
+            <span className="badge" onClick={toggleTags}>
+              ...
+            </span>
+          )}
+        </h2>
+        <ul className="recipe-card__nav">
+          <li onClick={getState}>
+            <h3 className={`${ingredientOn ? "active" : ""}`}>Ingredients</h3>
+          </li>
+          <li onClick={getState}>
+            <h3 className={`${infoOn ? "active" : ""}`}>Information</h3>
+          </li>
+        </ul>
+        <ul className="recipe-card__ingredients">
+          {ingredientOn &&
+            recipe.recipe.ingredients.map((ingre, i) => (
+              <li key={i} className="li">
+                {ingre.text}
+              </li>
+            ))}
+        </ul>
+
+        {infoOn && (
+          <ul className="recipe-card__ingredients">
+            <li>
+              Calories : <span>{recipe.recipe.calories}</span>
+            </li>
+            <li>
+              Meal Type : <span>{recipe.recipe.mealType}</span>
+            </li>
+            <li>
+              Cautions : <span>{recipe.recipe.cautions}</span>
+            </li>
+            <li>
+              Cuisine Type : <span>{recipe.recipe.cuisineType}</span>
+            </li>
+            <li>
+              Source : <span>{recipe.recipe.source}</span>
+            </li>
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
