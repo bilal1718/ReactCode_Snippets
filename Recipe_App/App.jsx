094a5a6624121c37e312e2f8fbaaaf10e12d34ed
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-13 15:51:19 +0500

    Update App.jsx

--- Recipe_App/App.jsx
+++ Recipe_App/App.jsx
@@ -1,68 +1,21 @@
 import React, { useState, useEffect } from "react";
-import "./App.css"
+import "./App.css";
 
-function App() {
-  const [recipeData, setRecipeData] = useState([]);
-  const [query, setQuery] = useState("");
-  const apiKey = "867c80f9ffb5e836c8d66abb429338ec";
-  const appId = "aa6401ab";
+function RecipeCard({ recipe, index }) {
+  const [ingredientOn, setIngredientOn] = useState(true);
+  const [infoOn, setInfoOn] = useState(false);
+  const [showMoreTags, setShowMoreTags] = useState(false);
+  const [numTagsDisplayed, setNumTagsDisplayed] = useState(5);
 
-  useEffect(() => {
+  const getRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+  };
 
-    fetch(`https://api.edamam.com/search?q=paratha&app_id=${appId}&app_key=${apiKey}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setRecipeData(data.hits);
-      })
-      .catch((error) => {
-        console.error("Error fetching recipe data:", error);
-      });
-  }, [query, apiKey, appId]);
-
-  return (
-    <>
-    <>
-  <link
-    href="https://fonts.googleapis.com/css?family=Open+Sans"
-    rel="stylesheet"
-  />
-  {recipeData && recipeData.map((recipe,i)=>(
-  <div className="recipe-card" key={i}>
-   <div
-      style={{
-        backgroundImage: `url(${recipe.recipe.image})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '50% 50%',
-        backgroundSize: 'cover',
-        height: 150
-      }}
-      />
-    <div className="recipe-card__body">
-      <h1 className="recipe-card__heading">{recipe.recipe.label}</h1>
-      <h2 className="recipe-card__subhead">
-        Crunchy around the edges, softer in the center, these oatmeal cookies
-        feature the nutty taste and nubbly texture of oats.{" "}
-      </h2>
-      <ul className="recipe-card__nav">
-        <li>
-          <h3 className="active">Ingredients</h3>
-        </li>
-        <li>
-          <h3>Method</h3>
-        </li>
-      </ul>
-      {recipe.recipe.ingredients.map((ingre)=>(
-      <ul className="recipe-card__ingredients">
-        <li>{ingre.text}</li>
-      </ul>
-      ))}
-    </div>
-  </div>
-  ))}
-</>
-</>
-  );
-}
-
-export default App;
+  const getContrastColor = (color) => {
+    const rgb = color.match(/\d+/g);
+    const brightness = (parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) / 1000;
+    return brightness >= 128 ? "black" : "white";
+  };
