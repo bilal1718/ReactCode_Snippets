39e3a75e4edb1c85d1adb437edafd9e17dfc0b95
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-13 15:52:08 +0500

    Update App.jsx

--- Recipe_App/App.jsx
+++ Recipe_App/App.jsx
@@ -102,3 +102,33 @@ const toggleTags = () => {
     </div>
   );
 }
+function App() {
+  const [recipeData, setRecipeData] = useState([]);
+  const apiKey = "867c80f9ffb5e836c8d66abb429338ec";
+  const appId = "aa6401ab";
+
+  useEffect(() => {
+    fetch(`https://api.edamam.com/search?q=paratha&app_id=${appId}&app_key=${apiKey}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setRecipeData(data.hits);
+      })
+      .catch((error) => {
+        console.error("Error fetching recipe data:", error);
+      });
+  }, [apiKey, appId]);
+
+  return (
+    <>
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+      {recipeData &&
+        recipeData.map((recipe, i) => (
+          <RecipeCard recipe={recipe} index={i} key={i} />
+        ))}
+    </>
+  );
+}
+
+export default App;
+
