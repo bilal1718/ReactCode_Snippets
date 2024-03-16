eddc004253abea3bf82388b5e621ea64e717abed
Muhammad Bilal <muhbil171845@gmail.com>
2024-03-16 17:43:42 +0500

    Update App.jsx

--- Recipe_App/App.jsx
+++ Recipe_App/App.jsx
@@ -10,3 +10,26 @@ function App() {
   const appId = "aa6401ab";
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  useEffect(() => {
+    if (searchTerm.trim() !== '') {
+      setLoading(true);
+      setError(null);
+      fetch(`https://api.edamam.com/search?q=${searchTerm}&app_id=${appId}&app_key=${apiKey}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setRecipeData(data.hits);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching recipe data:", error);
+          setError("Error fetching recipe data. Please try again later.");
+          setLoading(false);
+        });
+    }
+  }, [apiKey, appId, searchTerm]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchTerm(inputValue); // Set the search term for fetching data
+  }
